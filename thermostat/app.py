9cb92d68e49d79886ff59f39ac2b024fa3d37480
Vasja Volin <vasja.volin@gmail.com>
2016-10-05 01:50:35 -0400

    add more complete startup behavior for thermostat instance apps

--- thermostat/app.py
+++ thermostat/app.py
@@ -1,9 +1,15 @@
 import random
+import uuid
 from flask import Flask
 from flask_restful import Resource, Api
+import requests
 
 app = Flask(__name__)
 api = Api(app)
+
+HUB_HOST = 'localhost'
+HUB_PORT = 3001
+random_port = random.randrange(8001, 8099)
 
 class Thermostat(Resource):
     def get(self, target=None, current=None, history=None):
@@ -13,7 +19,16 @@ class Thermostat(Resource):
 
 api.add_resource(Thermostat, '/')
 
-random_port = random.randrange(8001, 8099)
+
+my_info = {
+    "ip": "localhost",
+    "port": random_port,
+    "id": uuid.uuid1(),
+}
 
 if __name__ == '__main__':
-      app.run(host='localhost', port=random_port)
+    # register this instance with the hub
+    requests.post(HUB_HOST + ':' + HUB_PORT, json=my_info)
+
+    # need to check if we can actually bind to this port
+    app.run(host='localhost', port=random_port)
