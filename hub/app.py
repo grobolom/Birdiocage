3ac4e9f04d3149520e785b34f66d239b57bd776e
Vasja Volin <vasja.volin@gmail.com>
2016-10-05 01:33:58 -0400

    add a fleshed out /thermostats GET endpoint to the hub api

--- hub/app.py
+++ hub/app.py
@@ -1,12 +1,28 @@
+from flask_pymongo import PyMongo
 from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "birdcage_hub"
+mongo = PyMongo(app, config_prefix='MONGO')
 api = Api(app)
 
 class Thermostat(Resource):
-    def get(self):
-        return ['get thermostats']
+    def get(self, id=None):
+        data = []
+
+        if id:
+            info = mongo.db.thermostat.find_one({"_id": id})
+            if info:
+                return jsonify({"status": "ok", "data": info})
+            else:
+                return {"response": "no thermostat registered under id {}".format(id)}
+
+        cursor = mongo.db.thermostat.find(None, {"_id":1,"name": 1})
+        for thermostat in cursor:
+            data.append(thermostat)
+        return jsonify({"response": data})
+
     def post(self):
         return ['post thermostats']
 
