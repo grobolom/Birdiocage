4430091ca16bd744d0441fde067c1a29d14543c8
Vasja Volin <vasja.volin@gmail.com>
2016-10-05 03:22:38 -0400

    work in some simple encoding for working with BSON

--- hub/app.py
+++ hub/app.py
@@ -1,5 +1,7 @@
+import json
+from bson import ObjectId
 from flask_pymongo import PyMongo
-from flask import Flask, jsonify, redirect, url_for
+from flask import Flask, jsonify, redirect, url_for, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -7,21 +9,28 @@ app.config["MONGO_DBNAME"] = "birdcage_hub"
 mongo = PyMongo(app, config_prefix='MONGO')
 api = Api(app)
 
+# ripped from StackOverflow to properly encode BSON
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
 class Thermostat(Resource):
     def get(self, id=None):
         data = []
 
         if id:
-            info = mongo.db.thermostat.find_one({"_id": id})
+            info = mongo.db.thermostat.find_one({"_id": ObjectId(id)})
             if info:
-                return jsonify({"status": "ok", "data": info})
+                return JSONEncoder().encode({"status": "ok", "data": info})
             else:
                 return {"response": "no thermostat registered under id {}".format(id)}
 
         cursor = mongo.db.thermostat.find(None, {"_id":1,"name": 1})
         for thermostat in cursor:
             data.append(thermostat)
-        return jsonify({"response": data})
+        return JSONEncoder().encode({"response": data})
 
     def post(self):
         return ['post thermostats']
@@ -42,6 +51,7 @@ class Registry(Resource):
         return redirect(url_for("thermostats"))
 
 api.add_resource(Thermostat, '/thermostats', endpoint="thermostats")
+api.add_resource(Thermostat, '/thermostats/<id>', endpoint="id")
 api.add_resource(Temperature, '/temperature')
 api.add_resource(Registry, '/register')
 
