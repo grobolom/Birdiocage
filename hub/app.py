0a9393e5409b3b8dca73dc03415f84a31e86c4ee
Vasja Volin <vasja.volin@gmail.com>
2016-10-05 01:12:22 -0400

    add dummy endpoints for our eventual hub application

--- hub/app.py
+++ hub/app.py
@@ -4,11 +4,23 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class Thermostat(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return ['get thermostats']
+    def post(self):
+        return ['post thermostats']
 
-api.add_resource(HelloWorld, '/')
+class Temperature(Resource):
+    def get(self):
+        return ['get average temp']
+
+class Registry(Resource):
+    def post(self):
+        return ['post new thermostat']
+
+api.add_resource(Thermostat, '/thermostats')
+api.add_resource(Temperature, '/temperature')
+api.add_resource(Registry, '/register')
 
 if __name__ == '__main__':
       app.run(host='localhost', port=3001)
